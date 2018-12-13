da4109efae92a82b46bb42ecd73f4af5b4839327
hodux928 <45477360+hodux928@users.noreply.github.com>
2018-12-13 17:55:35 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
+
 var links = [
     { url: 'http://google.com', anchor: 'google.com' },
     { url: 'http://bing.com', anchor: 'bing.com' },
     { url: 'http://google.com', anchor: 'google.com' }
-]
+];
